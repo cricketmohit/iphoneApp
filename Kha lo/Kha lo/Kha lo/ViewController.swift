3d6872b064e97d3e969c457d345e3eefd6a54467
Mohit Sharma <cricketmohit@gmail.com>
2018-09-18 15:45:51 +0200

    First IPhone App

---
+++ Kha lo/Kha lo/Kha lo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kha lo
+//
+//  Created by Mohit Sharma on 2018-08-14.
+//  Copyright © 2018 Mohit Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
